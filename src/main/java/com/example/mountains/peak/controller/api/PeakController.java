4657592476a01053af0bde7ca1781cdcdd46a2ed
Uladzislau Lukashevich <lukashevichuladzislau@gmail.com>
2023-11-06 01:01:31 +0100

    feat: controller for managing peaks

---
+++ src/main/java/com/example/mountains/peak/controller/api/PeakController.java
@@ -0,0 +1,16 @@
+package com.example.mountains.peak.controller.api;
+
+import com.example.mountains.peak.dto.GetPeakResponse;
+import com.example.mountains.peak.dto.GetPeaksResponse;
+import com.example.mountains.peak.dto.PutPeakRequest;
+import com.example.mountains.range.entity.Range;
+
+import java.util.UUID;
+
+public interface PeakController {
+
+    GetPeaksResponse getPeaks();
+    GetPeakResponse getPeak(UUID id);
+    void putPeak(UUID id, Range range, PutPeakRequest request);
+    void deletePeak(UUID id);
+}
---
+++ src/main/java/com/example/mountains/peak/controller/impl/PeakDefaultController.java
@@ -0,0 +1,57 @@
+package com.example.mountains.peak.controller.impl;
+
+import com.example.mountains.peak.controller.api.PeakController;
+import com.example.mountains.peak.dto.GetPeakResponse;
+import com.example.mountains.peak.dto.GetPeaksResponse;
+import com.example.mountains.peak.dto.PutPeakRequest;
+import com.example.mountains.peak.function.PeakToResponseFunction;
+import com.example.mountains.peak.function.PeaksToResponseFunction;
+import com.example.mountains.peak.function.RequestToPeakFunction;
+import com.example.mountains.peak.service.api.PeakService;
+import com.example.mountains.range.entity.Range;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@Controller
+public class PeakDefaultController implements PeakController {
+
+    private final PeakService service;
+    private final PeakToResponseFunction peakToResponse;
+    private final PeaksToResponseFunction peaksToResponse;
+    private final RequestToPeakFunction requestToPeak;
+
+    @Autowired
+    public PeakDefaultController(
+            PeakService service,
+            PeakToResponseFunction peakToResponse,
+            PeaksToResponseFunction peaksToResponse,
+            RequestToPeakFunction requestToPeak
+    ){
+        this.service = service;
+        this.peakToResponse = peakToResponse;
+        this.peaksToResponse = peaksToResponse;
+        this.requestToPeak = requestToPeak;
+    }
+    @Override
+    public GetPeaksResponse getPeaks() {
+        return peaksToResponse.apply(service.findAll());
+    }
+
+    @Override
+    public GetPeakResponse getPeak(UUID id) {
+        return service.find(id).map(peakToResponse).orElseThrow(() -> new NoSuchElementException());
+    }
+
+    @Override
+    public void putPeak(UUID id, Range range, PutPeakRequest request) {
+        service.create(requestToPeak.apply(id, range, request));
+    }
+
+    @Override
+    public void deletePeak(UUID id) {
+        service.delete(id);
+    }
+}
