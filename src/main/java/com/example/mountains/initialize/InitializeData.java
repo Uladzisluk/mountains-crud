a4534f6216fab3a60c55a30b683d01674a45764e
Uladzislau Lukashevich <lukashevichuladzislau@gmail.com>
2023-11-06 02:36:46 +0100

    fix: initialization fixed

--- src/main/java/com/example/mountains/initialize/InitializeData.java
+++ src/main/java/com/example/mountains/initialize/InitializeData.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -27,13 +28,13 @@ public class InitializeData implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         Range range1 = Range.builder().id(UUID.fromString("9ddbffa6-7c2d-11ee-b962-0242ac120002"))
-                .name("Black Forest").length(160)
+                .name("Black Forest").length(160).peaks(new ArrayList<>())
                 .build();
         Range range2 = Range.builder().id(UUID.fromString("9ddc01f4-7c2d-11ee-b962-0242ac120002"))
-                .name("Alps").length(1200)
+                .name("Alps").length(1200).peaks(new ArrayList<>())
                 .build();
         Range range3 = Range.builder().id(UUID.fromString("9ddc0334-7c2d-11ee-b962-0242ac120002"))
-                .name("Eifel").length(100)
+                .name("Eifel").length(100).peaks(new ArrayList<>())
                 .build();
 
         rangeService.create(range1);
@@ -41,31 +42,31 @@ public class InitializeData implements InitializingBean {
         rangeService.create(range3);
 
         peakService.create(Peak.builder().id(UUID.fromString("5064164a-7c2e-11ee-b962-0242ac120002"))
-            .name("Feldberg").height(1493).build());
+            .name("Feldberg").height(1493).range(range1).build());
         peakService.create(Peak.builder().id(UUID.fromString("5064191a-7c2e-11ee-b962-0242ac120002"))
-                .name("Belchen").height(1414).build());
+                .name("Belchen").height(1414).range(range1).build());
         peakService.create(Peak.builder().id(UUID.fromString("50641a5a-7c2e-11ee-b962-0242ac120002"))
-                .name("Herzogenhom").height(1415).build());
+                .name("Herzogenhom").height(1415).range(range1).build());
         peakService.create(Peak.builder().id(UUID.fromString("50641b72-7c2e-11ee-b962-0242ac120002"))
-                .name("Silbergberg").height(1358).build());
+                .name("Silbergberg").height(1358).range(range1).build());
 
         peakService.create(Peak.builder().id(UUID.fromString("a633f310-7c2e-11ee-b962-0242ac120002"))
-                .name("Alphubel").height(4206).build());
+                .name("Alphubel").height(4206).range(range2).build());
         peakService.create(Peak.builder().id(UUID.fromString("a633f61c-7c2e-11ee-b962-0242ac120002"))
-                .name("Dom").height(4545).build());
+                .name("Dom").height(4545).range(range2).build());
         peakService.create(Peak.builder().id(UUID.fromString("a633f928-7c2e-11ee-b962-0242ac120002"))
-                .name("Breithom").height(4164).build());
+                .name("Breithom").height(4164).range(range2).build());
         peakService.create(Peak.builder().id(UUID.fromString("a633fd10-7c2e-11ee-b962-0242ac120002"))
-                .name("Weissmies").height(4017).build());
+                .name("Weissmies").height(4017).range(range2).build());
 
         peakService.create(Peak.builder().id(UUID.fromString("bf1c2708-7c2e-11ee-b962-0242ac120002"))
-                .name("Radersberg").height(637).build());
+                .name("Radersberg").height(637).range(range3).build());
         peakService.create(Peak.builder().id(UUID.fromString("bf1c2942-7c2e-11ee-b962-0242ac120002"))
-                .name("Botrange").height(694.24).build());
+                .name("Botrange").height(694.24).range(range3).build());
         peakService.create(Peak.builder().id(UUID.fromString("bf1c2a46-7c2e-11ee-b962-0242ac120002"))
-                .name("Steling").height(658.3).build());
+                .name("Steling").height(658.3).range(range3).build());
         peakService.create(Peak.builder().id(UUID.fromString("bf1c2eba-7c2e-11ee-b962-0242ac120002"))
-                .name("Calmont").height(378.4).build());
+                .name("Calmont").height(378.4).range(range3).build());
     }
 
     @SneakyThrows
