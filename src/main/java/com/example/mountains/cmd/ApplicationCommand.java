81e9b466cbd7959677773daac5044c0eefc3a9b0
Uladzislau Lukashevich <lukashevichuladzislau@gmail.com>
2023-11-06 01:43:25 +0100

    feat: application command line runner with peaks management

---
+++ src/main/java/com/example/mountains/cmd/ApplicationCommand.java
@@ -0,0 +1,70 @@
+package com.example.mountains.cmd;
+
+import com.example.mountains.peak.dto.PutPeakRequest;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.example.mountains.peak.controller.api.PeakController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.UUID;
+
+@Component
+public class ApplicationCommand implements CommandLineRunner {
+    private final PeakController peakController;
+    private final ObjectWriter writer;
+
+    @Autowired
+    public ApplicationCommand(PeakController peakController, ObjectWriter writer){
+        this.peakController = peakController;
+        this.writer = writer;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        main_loop:
+        while(true){
+            command = scanner.next();
+            switch (command){
+                case "get_all" ->
+                    System.out.println(writer.writeValueAsString(peakController.getPeaks()));
+                case "get" -> {
+                    UUID uuid = UUID.fromString(scanner.next());
+                    try{
+                        System.out.println(writer.writeValueAsString(peakController.getPeak(uuid)));
+                    }catch (NoSuchElementException ex){
+                        System.out.println("NOT_FOUND");
+                    }
+                }
+                case "delete" -> {
+                    try{
+                        UUID uuid = UUID.fromString(scanner.next());
+                        peakController.deletePeak(uuid);
+                    }catch (NoSuchElementException ex){
+                        System.out.println("NOT_FOUND");
+                    }
+                }
+                case "put" -> {
+                    UUID uuid = UUID.fromString(scanner.next());
+                    PutPeakRequest request = PutPeakRequest.builder()
+                            .name(scanner.next())
+                            .height(scanner.nextDouble())
+                            .build();
+                    try{
+                        peakController.putPeak(uuid, request);
+                    }
+                    catch (IllegalArgumentException ex){
+                        System.out.println("BAD_REQUEST");
+                    }
+                }
+                case "quit" -> {
+                    break main_loop;
+                }
+            }
+        }
+    }
+}
--- src/main/java/com/example/mountains/peak/dto/PutPeakRequest.java
+++ src/main/java/com/example/mountains/peak/dto/PutPeakRequest.java
@@ -1,7 +1,9 @@
 package com.example.mountains.peak.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
+@Builder
 @Data
 public class PutPeakRequest {
     String name;
