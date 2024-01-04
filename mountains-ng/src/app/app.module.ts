c7a0beed446c75fc9d9312504501fee17bab2d9f
Uladzislau Lukashevich <lukashevichuladzislau@gmail.com>
2024-01-04 12:14:47 +0100

    feat: main component content

--- mountains-ng/src/app/app.module.ts
+++ mountains-ng/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
+import { MainComponent } from './component/main/main.component';
+import { NavComponent } from './component/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
