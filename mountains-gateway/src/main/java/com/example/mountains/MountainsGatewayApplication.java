118abff1c9abe5c1a0ae216fed1c90c7284b97e8
Uladzislau Lukashevich <lukashevichuladzislau@gmail.com>
2024-01-04 06:08:09 +0100

    chore: CORS configuration for browser client

--- mountains-gateway/src/main/java/com/example/mountains/MountainsGatewayApplication.java
+++ mountains-gateway/src/main/java/com/example/mountains/MountainsGatewayApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class MountainsGatewayApplication {
@@ -43,5 +50,18 @@ public class MountainsGatewayApplication {
                         )
                         .uri(peakUrl))
                 .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+        corsConfig.addAllowedHeader("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return new CorsWebFilter((CorsConfigurationSource) source);
     }
 }
